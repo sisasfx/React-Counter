aa1eca646f3154429cd810e5c3cb92adaf74d795
Miquel Ortiz Graupera <sisasfx@gmail.com>
2023-02-28 21:31:02 +0000

    Done without BONUS

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {isBoolean} from "../index.js"
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+
+const Counter = (props) => {
+  return (
+    <div className="container">
+        <h1>{props.status}</h1>
+    <div className="row d-flex-inline justify-content-center">
+      <div className="card d-flex justify-content-center align-items-center">
+        <FontAwesomeIcon icon={faClock} id="icon"/>
+      </div>
+      <div className="card d-flex justify-content-center align-items-center">
+        <h1 className="card-text">{props.dias}</h1>
+      </div>
+      <div className="card d-flex justify-content-center align-items-center">
+        <h1 className="card-text">{props.horas}</h1>
+      </div>
+      <div className="card d-flex justify-content-center align-items-center">
+        <h1 className="card-text">{props.minutos}</h1>
+      </div>
+      <div className="card d-flex justify-content-center align-items-center">
+        <h1 className="card-text">{props.segundos}</h1>
+      </div>
+    </div>
+    <div className="row d-flex justify-content-center">
+        <button onClick={isBoolean} id="btn">STOP</button>
+        <button onClick={isBoolean} id="btn">START</button>
+    </div>
+    </div>
+
+  );
+};
+
+// TYPE my props
+Counter.PropTypes = {
+  segundos: PropTypes.number,
+  minutos: PropTypes.number,
+  horas: PropTypes.number,
+  dias: PropTypes.number,
+  status: PropTypes.string
+};
+// INIT default props
+Counter.defaultProps = {
+  segundos: 0,
+  minutos: 0,
+  horas: 0,
+  dias: 0,
+  status: "Running"
+};
+
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,54 @@
 //import react into the bundle
+import { counter } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Counter  from "../js/component/counter.jsx"
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+//INIT variables
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let dias = 0;
+let stopCounter = true;
+
+//Function with setIterval on it
+
+const renderizarComponente = () => {
+    setInterval(() => {
+        if(segundos >= 59){
+            segundos = 0
+            minutos++
+        }else if(minutos >= 59){
+            horas++
+            minutos = 0
+        }else if(horas > 23){
+            dias++
+            horas = 0
+        }else{
+            segundos++
+        }
+        ReactDOM.render(<Counter segundos={segundos} minutos={minutos} horas={horas} dias={dias} />, document.querySelector("#app"));
+    },1000)
+}
+
+// Ask for boolean to change the Render status and stop counting
+const isBoolean = () =>{
+    console.log("El valor init --> ",stopCounter)
+    stopCounter = !stopCounter
+    console.log("El valor end --> ",stopCounter)
+}
+
+// RENDER -->TO FIX:  if Boolean change, doesn't affect the Render
+if(stopCounter){
+    renderizarComponente()
+}else{
+    ReactDOM.render(<Counter segundos={segundos} minutos={minutos} horas={horas} dias={dias}/>, document.querySelector("#app"))
+}
+
+export  {renderizarComponente, isBoolean}
+
+
+
